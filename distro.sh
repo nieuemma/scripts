737d1064389ff8c67cdb9046fbbbd9663695fedb
sedorjulian <149756597+sedorjulian@users.noreply.github.com>
2025-04-18 14:54:38 -0400

    Update distro.sh

--- distro.sh
+++ distro.sh
@@ -4,17 +4,49 @@
 detect_distro() {
     if [ -f /etc/os-release ]; then
         . /etc/os-release
-        echo "You are using $NAME $VERSION"
+        distro=$NAME
     elif [ -f /etc/lsb-release ]; then
         . /etc/lsb-release
-        echo "You are using $DISTRIB_ID $DISTRIB_RELEASE"
+        distro=$DISTRIB_ID
     elif [ -f /etc/redhat-release ]; then
-        echo "You are using $(cat /etc/redhat-release)"
+        distro=$(cat /etc/redhat-release)
     elif [ -f /etc/issue ]; then
-        echo "You are using $(head -n 1 /etc/issue)"
+        distro=$(head -n 1 /etc/issue)
     else
         echo "Could not detect the Linux distribution. Unknown or unsupported system."
+        distro="Unknown"
     fi
+        echo "You are using $distro"
+}
+
+# Function to run commands based on the distro
+run_commands_based_on_distro() {
+    case "$distro" in
+        *Ubuntu*)
+            echo "Running commands for Ubuntu..."
+            # Add your Ubuntu-specific commands here
+            sudo apt install curl
+            ;;
+        *Debian*)
+            echo "Running commands for Debian..."
+            # Add your Debian-specific commands here
+            sudo apt install curl
+            ;;
+        *Fedora*)
+            echo "Running commands for Fedora..."
+            # Add your Fedora-specific commands here
+            sudo dnf install curl
+            ;;
+        *CentOS*)
+            echo "Running commands for CentOS..."
+            # Add your CentOS-specific commands here
+            sudo yum install -y curl
+            ;;
+        *)
+            echo "No specific commands for this distribution."
+            ;;
+    esac
 }
 
 detect_distro
+run_commands_based_on_distro
