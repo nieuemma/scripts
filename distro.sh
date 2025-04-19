870175ff3eee9abade17a66dc0f5c736328967e3
nieuemma <149756597+nieuemma@users.noreply.github.com>
2025-04-18 23:46:19 -0400

    Update distro.sh

--- distro.sh
+++ distro.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# Function to detect the Linux distribution
+# Exit on error
+set -euxo pipefail
+# Define packages to install
+DEB_PKG="gnome-shell nautilus epiphany-browser gnome-terminal gnome-control-center gnome-tweaks gnome-keyring xdg-user-dirs gdm3 network-manager network-manager-gnome btrfs-progs neovim"
+RHL_PKG="gnome-shell nautilus epiphany gnome-terminal gnome-control-center gnome-tweaks gnome-keyring xdg-user-dirs gdm NetworkManager network-manager-applet btrfs-progs neovim"
+ARCH_PKG="gnome-shell nautilus epiphany gnome-console gnome-control-center gnome-tweaks gnome-keyring xdg-user-dirs gdm networkmanager nm-connection-editor btrfs-progs neovim"
+
+# Detect the active Linux distro
 distro_detect() {
     if [ -f /etc/os-release ]; then
         . /etc/os-release
@@ -8,29 +15,29 @@ distro_detect() {
         . /etc/lsb-release
         distro=$DISTRIB_ID
     elif [ -f /etc/redhat-release ]; then
-        distro=$(cat /etc/redhat-release)
+        distro="$(cat /etc/redhat-release)"
     elif [ -f /etc/issue ]; then
-        distro=$(head -n 1 /etc/issue)
+        distro="$(head -n 1 /etc/issue)"
     else
         echo "Could not detect the Linux distribution. Unknown or unsupported system."
         distro="Unknown"
     fi
 }
 
-# Function to run commands based on the distro
+# Install commands based on active Linux distro
 distro_commands() {
     case "$distro" in
         *Debian*|*Ubuntu*)
-            sudo apt install -y gnome-shell nautilus epiphany-browser gnome-terminal gnome-control-center gnome-tweaks gnome-keyring xdg-user-dirs gdm3 network-manager network-manager-gnome btrfs-progs neovim
+            sudo apt install -y $DEB_PKG
             ;;
         *Fedora*)
-            sudo dnf install -y gnome-shell nautilus epiphany gnome-terminal gnome-control-center gnome-tweaks gnome-keyring xdg-user-dirs gdm NetworkManager network-manager-applet btrfs-progs neovim
+            sudo dnf install -y $RHL_PKG
             ;;
         *CentOS*)
-            sudo yum install -y gnome-shell nautilus epiphany gnome-terminal gnome-control-center gnome-tweaks gnome-keyring xdg-user-dirs gdm NetworkManager network-manager-applet btrfs-progs neovim
+            sudo yum install -y $RHL_PKG
             ;;
         *"Arch Linux"*)
-            sudo pacman -S --noconfirm --needed gnome-shell nautilus epiphany gnome-console gnome-control-center gnome-tweaks gnome-keyring xdg-user-dirs gdm networkmanager nm-connection-editor baobab gnome-disk-utility gnome-text-editor gnome-system-monitor loupe totem decibels guake neovim btrfs-progs timeshift
+            sudo pacman -S --noconfirm --needed $ARCH_PKG
             [ -f /bin/btrfs-list ] && echo "btrfs-list is already installed." || (git clone https://aur.archlinux.org/btrfs-list.git && cd btrfs-list && makepkg -s --install --noconfirm *.zst && cd .. && rm -rf btrfs-list)
             ;;
         *Unknown*)
